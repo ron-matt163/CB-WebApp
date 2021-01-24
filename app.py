02f76fcaa008586e8e757d3b20cef1195dfabac3
ron-matt163 <rohanjm01@gmail.com>
2021-01-25 01:37:10 +0530

    Added trending topics tweet collection

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify,render_template,redirect,url_for,flash
 import os
 import pickle
+import json
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 import re
@@ -10,6 +11,8 @@ from nltk.corpus import stopwords
 import string
 import tweepy 
 import sys
+import geocoder
+from langdetect import detect
 # Fill the X's with the credentials obtained by  
 # following the above mentioned procedure. 
 
@@ -25,6 +28,14 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify = True
 
 
 app=Flask(__name__)
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
 
 app.config['SECRET_KEY'] = os.urandom(24)
 
@@ -83,9 +94,9 @@ def printstats(query,tweet_limit):
     if st_count == 0:
         tweet_limit = int(tweet_limit)
         print("Tweets to be collected with search keyword: " + query)
-        date_since = '2021-01-23'
+        date_since = '2021-01-24'
         c=0
-        tweets = tweepy.Cursor(api.search, q=query,lang="en", since=date_since,result_type='popular', timeout=999999).items(10)
+        tweets = tweepy.Cursor(api.search, q=query,lang="en", since=date_since,result_type='popular', timeout=999999).items(50)
         for tweet in tweets:
             print("Tweet:\n")
             print(tweet.text)
@@ -95,7 +106,7 @@ def printstats(query,tweet_limit):
             print("\nPoster: " + poster + "\n")
             print("Replies to this tweet: \n")
             count = 0
-            for reply in tweepy.Cursor(api.search,q="@"+poster, since_id=tweet_id).items(1000):
+            for reply in tweepy.Cursor(api.search,q="@"+poster, since_id=tweet_id).items(200):
                 if(reply.in_reply_to_status_id == tweet_id):
                     tweet_count += 1
                     input_text = []
@@ -170,10 +181,85 @@ def sendmessagesconfirm():
 
     return render_template('send_messages_confirm.html',offenders=offenders,victims=victims)
 
+@app.route('/location',methods=['GET','POST'])
+def location():
+    return render_template("location.html")
 
-@app.route('/tttc')
-def trendingtopictweetcollection(): 
-    return render_template("trending-topic-tweet-collection.html")    
+@app.route('/tttc',methods=['GET','POST'])
+def trendingtopictweetcollection():
+    location=''
+    trends_in_location = []
+    if request.method == 'POST':
+        locationList = request.form.getlist('location')
+        location = locationList[0]
+        date_since="2020-01-24"
+        available_loc = api.trends_available()
+        # writing a JSON file that has the available trends around the world
+        with open("available_locs_for_trend.json","w") as wp:
+            wp.write(json.dumps(available_loc, indent=1))
+
+        # Trends for Specific Country     # location as argument variable 
+        
+        g = geocoder.osm(location) # getting object that has location's latitude and longitude
+
+        closest_loc = api.trends_closest(g.lat, g.lng)
+        trends = api.trends_place(closest_loc[0]['woeid'],count=30,lang="en")
+        # writing a JSON file that has the latest trends for that location
+        print("The top trends for the location are :") 
+        for value in trends: 
+            for trend in value['trends']:
+                if isEnglish(trend['name']):
+                    trends_in_location.append(trend['name'])
+    return render_template("trending-topic-tweet-collection.html",trends_in_location = trends_in_location,location=location)
+
+@app.route('/showtweetstttc',methods=['GET','POST'])
+def showtweetstttc():
+    st_count = 0
+    bully_records = []
+    tweet_count = 0
+    bully_count = 0
+    percentage = 0.0
+    date_since="2020-01-24"
+    tweet_limit = 15
+    if request.method == 'POST':
+        trends = request.form.getlist('selectedTrends')
+        for trend in trends:
+            tweets = tweepy.Cursor(api.search, q=trend,lang="en", since=date_since,result_type='popular', timeout=999999).items(50)
+            for tweet in tweets:
+                print("Tweet:\n")
+                print(tweet.text)
+                tweet_id = tweet.id
+                print("Tweet ID: " + str(tweet_id))
+                poster = tweet.user.screen_name
+                print("\nPoster: " + poster + "\n")
+                print("Replies to this tweet: \n")
+                count = 0
+                for reply in tweepy.Cursor(api.search,q="@"+poster, since_id=tweet_id).items(1000):
+                    if(reply.in_reply_to_status_id == tweet_id):
+                        tweet_count += 1
+                        input_text = []
+                        print("Tweet index: "+str(tweet_count))
+                        input_text.append(reply.text)
+                        print(input_text)
+                        input_vector = loaded_func_countvectorizer.transform(input_text)
+                        result = loaded_NB_classifier.predict(input_vector)
+                        print("\nClassification:\n")
+                        print(result)
+                        if(result[0]):
+                            bully_row = []
+                            bully_count += 1
+                            bully_row.append(reply.user.screen_name)
+                            bully_row.append(poster)
+                            bully_row.append(reply.text)
+                            bully_records.append(bully_row)
+                    if tweet_count>=tweet_limit:
+                        break    
+                if tweet_count>=tweet_limit:
+                    break
+            percentage = round((100*float(bully_count)/tweet_count),2)  
+            st_count +=1
+    return render_template('show_tweets_tttc.html',bully_tweets=bully_records,tweet_count=tweet_count,bully_count=bully_count,percentage=percentage)
+
 
 if __name__ == "__main__":
     try:
